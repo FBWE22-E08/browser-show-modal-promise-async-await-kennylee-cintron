422ecf6f90e2391fd0bf9e985f6da53af74c6ab1
Kennylee Cintron <user@user-thinkpad-l15.fritz.box>
2023-01-30 16:04:21 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./css/style.css"; //importing css
+
+const box = document.querySelector(".modal");
+
+const thePromise = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("block");
+    }, 60000);
+  });
+};
+
+const resolvingBoxPromise = async () => {
+  let result = await thePromise();
+  box.style.display = result;
+};
+
+resolvingBoxPromise();
